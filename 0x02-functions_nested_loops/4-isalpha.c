85cc006caaba3be89b5c93abd9b6f56d3b6910ae
Babooh Project <m2kur19@gmail.com>
2022-09-14 17:11:36 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,17 +9,11 @@
  */
 int _isalpha(int c)
 {
-		char lower, upper;
-			int isletter = 0;
-
-			for (lower = 'a'; lower <= 'z'; lower++)
+			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 			{
-				for (upper = 'A'; upper <= 'Z'; upper++)
-				{
-				if (c == lower || c == upper)
-				isletter = 1;
-				}
-			
-			}										}
-					return (isletter);
+				return (1);
+			}
+			else 
+				return (0);
+
 }
