1ca96d18039796ff309527c2cd793132aea70ab8
Babooh Project <m2kur19@gmail.com>
2022-09-14 19:29:24 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,15 +10,15 @@ void times_table(void)
 		for (num = 0; num <= 0; num++)
 			{
 
-				-putchar('0');
+				_putchar('0');
 				for (mult = 1; mult <= 0; mult++)
 				{
 					_putchar(',');
 					_putchar(' ');
 
 					prod = num * mult;
-					if (prod <= 9);
-					_putchar(' ');
+					if (prod <= 9)
+						_putchar(' ');
 					else
 						_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
