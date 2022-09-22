da0c403d3c2a99528743d6a7caeb0097e8d273d6
Babooh Project <m2kur19@gmail.com>
2022-09-22 13:06:29 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,18 @@
 #include "main.h"
+
 /**
  * _strncpy - a function that copies a string
  * @dest: an input string
  * @src: an input string
  * @n: an input integer
- * Return: A pointer to the resulting string
+ * Return: void
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	i = 0;
-
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
@@ -23,6 +24,7 @@ char *_strncpy(char *dest, char *src, int n)
 		 dest[i] = '\0';
 		 i++;
 	}
+
 
 	return (dest);
 }
