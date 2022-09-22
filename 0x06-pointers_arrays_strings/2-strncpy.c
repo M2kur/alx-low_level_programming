b1836338bd6b9b984826d2046369bef0c80380ae
Babooh Project <m2kur19@gmail.com>
2022-09-22 11:31:01 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,17 +9,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-       
+
 	i = 0;
 
-	while (i < n && src{i} i= '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
+
 	}
 	while (i < n)
 	{
-		 dest[i] =n'\0';
+		 dest[i] = '\0';
 		 i++;
 	}
 
