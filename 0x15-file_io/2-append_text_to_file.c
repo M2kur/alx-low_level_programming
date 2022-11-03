e93f986369633db260677babbede29856ce240d5
M2kur <m2ku19@gmail.com>
2022-11-03 13:37:03 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	hile (*(text_content + len))
+	while (*(text_content + len))
 		len++;
 
 	fdw = write(fdo, text_content, len);
